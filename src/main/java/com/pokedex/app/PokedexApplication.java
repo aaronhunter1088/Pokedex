d64fbd08435d4bd6f52e637daaa7a2defd2e62fd
Michael Ball <aaronhunter@live.com>
2025-07-13 16:05:21 -0500

    started war from command line using parent pom

--- src/main/java/com/pokedex/app/PokedexApplication.java
+++ src/main/java/com/pokedex/app/PokedexApplication.java
@@ -1,10 +1,13 @@
 package com.pokedex.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class PokedexApplication extends SpringBootServletInitializer {
 
--- src/main/java/com/pokedex/app/controllers/PokemonListController.java
+++ src/main/java/com/pokedex/app/controllers/PokemonListController.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 @Controller
 public class PokemonListController extends BaseController {
